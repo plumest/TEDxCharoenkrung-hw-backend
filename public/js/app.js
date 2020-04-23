ef1395ddf6604743349e6f489e0d3bb8ae303d8d
plumest <chayathon.k@ku.th>
2020-04-23 19:32:17 +0700

    Adding new Collection feature

--- public/js/app.js
+++ public/js/app.js
@@ -35,30 +35,48 @@ Vue.component('kanban-list', {
 
 Vue.component('kanban-collection', {
     props: {
-        collection: Array,
+        collection: Array
+    },
+    data: () => {
+        return { newCollection: '' }
     },
     template: `
     <b-card-group 
         deck 
-        style="max-width: 20rem;"
         border-variant="primary"
         header="Primary"
         header-bg-variant="primary"
         header-text-variant="white"
         align="center">
-        <b-card v-for="(item, index) in collection" :key="index">
+        <b-card v-for="(item, index) in collection" :key="index" style="max-width: 20rem;">
             <template v-slot:header>
                 <h4 class="mb-0">{{ item.name }}</h4>
             </template>
             <kanban-list :list="item.list" :collectionIndex="index" @addTask="addTask"></kanban-list>
         </b-card>
+        <b-card style="max-width: 20rem;">
+            <b-form @submit="handleNewCollection">
+                <b-form-input  
+                    id="collection"
+                    v-model="newCollection"
+                    type="text"
+                    required
+                ></b-form-input>
+            </b-form>
+        </b-card>
     </b-card-group>
     `,
     methods: {
         addTask(task) {
-            // let { name, description, collectionIndex } = task;
-            // this.collection[collectionIndex].list.push({name: name, description: description});
             socket.emit('task', {task});
+        },
+        handleNewCollection(e) {
+            e.preventDefault();
+            console.log(this.newCollection);
+            let collection = {name: this.newCollection, list: []};
+            console.log(JSON.stringify(collection));
+            socket.emit('collection', {collection});
+            this.newCollection = "";
         }
     }
 });
@@ -67,12 +85,16 @@ const app = new Vue({
    el: '#app',
    data: () => {
        return {
-           collection: [{name: "Todo", list: [{name: "Hello 1", description: "Lorem 50"},{name: "Hello 2", description: "Lorem 50"},{name: "Hello 3", description: "Lorem 50"}]}]
+           collection: [
+               {name: "Todo 1", list: [{name: "Hello 1", description: "Lorem 50"},{name: "Hello 2", description: "Lorem 50"},{name: "Hello 3", description: "Lorem 50"}]},
+               {name: "Todo 2", list: [{name: "Hello 1", description: "Lorem 50"},{name: "Hello 2", description: "Lorem 50"},{name: "Hello 3", description: "Lorem 50"}]}
+               ]
        }
    },
     created() {
        socket.on('collection', data => {
-           this.collection.push(data);
+           this.collection.push(data.collection);
+           console.log(JSON.stringify(this.collection));
        });
        socket.on('task', task => {
            let { name, description, collectionIndex } = task.task;
